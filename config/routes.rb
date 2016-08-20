e713db91cb84fe82969188a0ac6274155bac3118
Sanjna Verma <sanjnav17@gmail.com>
2016-08-19 18:21:41 -0700

    new default page
    This was created earlier when you ran the controller generator
    (bin/rails generate controller Welcome index).

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,7 +56,9 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   Rails.application.routes.draw do
-    root 'application#hello'
+    get 'welcome/index'
+
+    root 'welcome#index'
   end
 
 end
